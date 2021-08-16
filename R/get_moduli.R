b35eb3358e278145bbdd78e7843587bd8239f1a3
Artur <artur.bicalho.s@gmail.com>
2021-08-16 16:01:58 -0400

    added documentation to get_moduli

--- R/get_moduli.R
+++ R/get_moduli.R
@@ -1,19 +1,78 @@
+#' Computes moduli space
+#' 
+#' Here, by moduli space we mean a metric space where each point represents an embedding
+#' of cells by PCA with a different choice of features, and each choice of features is a
+#' combination of gene clusters. By default, computes PCA embeddings of all nonempty 
+#' combinations of gene clusters.
+#' 
+#' 
+#' @param seuratObject A Seurat object
+#' @param gene.membership An integer vector with gene cluster assignments ans genes as names
+#' @param gene.clusters Clusters of genes as a list of character vectors, these clusters may
+#' intersect (the user must give exactly one the arguments gene.membership or gene.clusters)
+#' @param assay Assay in which to run PCA (DefaultAssay(seuratObject) by default). The assay must have a
+#' "scale.data" slot
+#' @param npcs Number of principal components (defaults to 30)
+#' @param weight.by.var Weight the cell embedding by variance of each PC (defaults to True)
+#' @param point Points to be considered as a list of integer vectors. If NULL all nonempty subsets of the
+#' set of clusters will be considered (defaults to NULL)
+#' @param n.cores Number of cores to use
+#' @param n.pts Number of points to subsample, if 0 all points are taken (default is 0)
+#' @param n.cells Number of cells to subsample when computing distances in the moduli space, if 0 all
+#' cells are used (default is 0).
+#' @param stride Number of pairs of cells to use in one iteration when computing the moduli metric. Taking
+#' larger strides is more efficient but requires more memory (default is 10000)
+#' @param seed Random seed to use (default is 42)
+#' @param filebacked Use a filebacked bigmatrix to store embeddings (default is TRUE)
+#' @param persist Keep bigmatrix with embeddings (default is FALSE)
+#' @param verbose Print time stamps (default is TRUE)
+#' 
+#' @return An object of class moduli with the following components:
+#' \item{gene.clusters}{A data.frame with gene clusters}
+#' \item{points}{A data.frame with point metadata}
+#' \item{metric}{A 'dist' object with the moduli metric}
+#' \item{seurat}{The Seurat object used to contruct the moduli}
+#' \item{assay}{Name of the assay used to construct the moduli}
+#' \item{cells}{Character vector with names of cells used to compute metric}
+#' \item{npcs}{Number of principal components used in moduli construction}
+#' 
+#' @details Computing the moduli space is very memory and processing intensive, specially
+#' computing the distances in the moduli space. The time complexity of the algorithm is
+#' \deqn{O(n.cells^2 n.pts^2 npcs + n.cells^2 n.pts^2)}
+#' and by default \eqn{n.pts = 2^{n.clusters} - 1}, where \eqn{n.clusters} is the number of  gene clusters.
+#' The matrix with embeddings has about \eqn{8*n.pts*npcs*n.cells*10^{-9}} GB, and in
+#' computing distances, each core will use need about \eqn{8*stride*n.pts*10^{-9}} GB.
+#' 
+#' @examples
+#' library(Seurat)
+#' data("pbmc_small")
+#' 
+#' # normalize data
+#' pbmc_small <- SCTransform(pbmc_small, verbose = F)
+#' 
+#' # cluster genes
+#' pam <- gene_medioid_clustering(pbmc_small, 4)
+#' 
+#' # compute moduli
+#' moduli <- get_moduli(pbmc_small, membership = pam$clustering, npcs = 5, verbose = F, filebacked = F)
+#' 
+#' 
 #' @export
-get_moduli <- function(seuratObject, membership = NULL, gene.clusters = NULL,
+get_moduli <- function(seuratObject, gene.membership = NULL, gene.clusters = NULL,
                        assay = DefaultAssay(seuratObject), npcs = 30, weight.by.var = T,
                        points = NULL, n.cores = 1, n.pts = 0, n.cells = 0, stride = 10000,
-                       seed = 123, filebacked = T, persist = F, verbose = T){
+                       seed = 42, filebacked = T, persist = F, verbose = T){
   # gene.clusters : clusters of genes as a list of vectors of gene names
-  if(sum(c(is.null(membership), is.null(gene.clusters))) != 1){
-    stop("Error: provide exatcly one of the arguments membership or gene.clusters")
+  if(sum(c(is.null(gene.membership), is.null(gene.clusters))) != 1){
+    stop("Error: provide exatcly one of the arguments gene.membership or gene.clusters")
   }
   
-  if(!is.null(membership)){
+  if(!is.null(gene.membership)){
     gene.clusters <- list()
-    cluster.ids <- sort(unique(membership))
+    cluster.ids <- sort(unique(gene.membership))
     gene.clusters <- list()
     for(k in cluster.ids){
-      gene.clusters[[k]] <- names(membership[membership == k])
+      gene.clusters[[k]] <- names(gene.membership[gene.membership == k])
     }
   }else{
     cluster.ids <- seq_along(gene.clusters)
@@ -79,7 +138,7 @@ get_moduli <- function(seuratObject, membership = NULL, gene.clusters = NULL,
     
     #based on Seurat's RunPCA
     if (length(feature.subset) > npcs*2){
-      set.seed(42) #seed used in Seurat
+      set.seed(seed)
       pca.results <- irlba(t(scaled.data[feature.subset,]), nv = npcs)
       if(weight.by.var){
         cell.embeddings <- pca.results$u %*% diag(pca.results$d)
