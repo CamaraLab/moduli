11874dff835142ca83e10b4d8b31363ed29ab8e3
Artur <artur.bicalho.s@gmail.com>
2021-08-17 10:22:33 -0400

    documented wich_point and point_metadata

--- R/utilities.R
+++ R/utilities.R
@@ -11,8 +11,23 @@ update_moduli <- function(moduli){
   return(out)
 }
 
-
-
+#' Retrieve ids of points
+#' 
+#' Retrieves ids of points using a set of gene clusters.
+#' 
+#' @param moduli A moduli object
+#' @param gene.clusters vector with ids of gene clusters
+#' @param filter a choice of filter, default value is \code{"equal"}. See details.
+#' 
+#' @return A vector with point ids
+#' 
+#' @details The filter \code{"equal"} gets the point that represents the point representing
+#' the clusters in \code{gene.clusters}. The option \code{"contains"} will get all points that
+#' that representing subsets of \code{gene.clusters}, while \code{"is_subset"} will get all
+#' points that have all elements of \code{gene.clusters}. The filter \code{"intersetcs"} retrieves
+#' ids of points that have any gene cluster in \code{gene.clusters}, and \code{"in_complement"}
+#' does the opposite, retrieving ids of points that only represent clusters not in \code{gene.clusters}.
+#' 
 #' @export
 which_point <- function(moduli, gene.clusters,
                         filter = c("equal", "contains", "is_subset", "intersects", "in_complement")){
@@ -23,12 +38,22 @@ which_point <- function(moduli, gene.clusters,
             contains = all(pt.clusters %in% gene.clusters),
             is_subset = all(gene.clusters %in% pt.clusters),
             intersects = any(pt.clusters %in% gene.clusters),
-            in_complement = !any(gene.clusters %in% pt.clusters)
+            in_complement = !any(pt.clusters %in% gene.clusters)
     )
   }
   return(moduli$points$id[sapply(moduli$points$clusters, f)])
 }
 
+#' Retrieve point metadata
+#' 
+#' Retrieves metadata of moduli point
+#' 
+#' @param moduli A moduli object
+#' @param points vector with ids of points to be retrieved, will retrieve all points by default
+#' 
+#' @return A data.frame with point ids, gene clusters represented and, if available,
+#' the analysis cluster the point belongs to
+#' 
 #' @export
 point_metadata <- function(moduli, points = moduli$points$id){
   out <- moduli$points[moduli$points$id %in% points,]
