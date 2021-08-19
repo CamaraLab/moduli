72aaa3687a5a4e8f3ca4d912606a5946c223e18a
Artur <artur.bicalho.s@gmail.com>
2021-08-19 18:17:03 -0400

    leiden is now suggested

--- R/moduli_clustering.R
+++ R/moduli_clustering.R
@@ -1,39 +1,39 @@
 
 
-#' Clusters moduli space using the Leiden algorithm
+#' Clusters moduli space using the Louvain or the Leiden algorithm
 #' 
-#' Create clusters of analysis (i.e. points in the moduli) by applying 
-#' \link[leiden]{leiden} to the snn graph of the moduli.
+#' Create clusters of analysis (i.e. clusters of points in the moduli) by applying 
+#' \link[igraph]{cluster_louvain} or \link[leiden]{leiden} to the snn graph of the moduli.
 #' 
 #' @param moduli A moduli object with a graph in the \code{snn.graph} slot
-#' @param resolution_paramenter Resolution passed to the Leiden algorithm (see \link[leiden]{leiden}), default
-#' value is 1
-#' @param partition_type Type of partition to use (see \link[leiden]{leiden}), default value is
-#' "RBConfigurationVertexPartition"
+#' @param algorithm Which algorithm to use, the options are "Louvain" and "Leiden", default value is
+#' "Louvain"
+#' @param ... Additional arguments passed to \link[leiden]{leiden}
 #' @param  enrich Whether to enrich the analysis clusters with differentially expressed gene clusters
 #' by calling \link{enrich_analyis_clusters}. Default value is TRUE
 #' @param thld Significance threshold for gene clusters, only used if \code{enrich} is TRUE
 #' @param seed Random seed, default value is 123
-#' @param ... Additional arguments passed to \link[leiden]{leiden}
-#' 
 #' 
 #' @return A moduli object with analysis clusters saved in the \code{analysis.clusters} slot.
 #' 
 #' @export
-cluster_moduli_space <- function(moduli, resolution_parameter = 1,
-                                 partition_type = "RBConfigurationVertexPartition",
-                                 enrich = T, thld = 0.05, seed = 123, ...){
+cluster_moduli_space <- function(moduli, algoritm = c("Louvain", "Leiden"), ...,
+                                 enrich = T, thld = 0.05, seed = 123){
   if(is.null(moduli$snn.graph)){
     stop("Error: snn graph required, run get_snn first")
   }
+  algoritm <- match.arg(algoritm)
+  if(algoritm == "Louvian"){
+    set.seed(seed)
+    communities <- igraph::cluster_louvain(moduli$snn.graph)
+    membership <- communities$membership
+  } else if(algoritm == "Leiden"){
+    if(!requireNamespace("leiden", quietly = TRUE)){
+      stop("Error: package TomKellyGenetics/leiden required")
+    }
+    membership <- leiden::leiden( moduli$snn.graph, seed = seed, ...)
+  }
   
-  membership <- leiden::leiden(
-    moduli$snn.graph,
-    partition_type = partition_type,
-    resolution_parameter = resolution_parameter,
-    seed = seed,
-    ...
-  )
   out <- moduli
   out$analysis.clusters <- data.frame(id = sort(unique(membership)))
   out$analysis.clusters$points <- lapply(out$analysis.clusters$id,
